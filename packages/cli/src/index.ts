bb558ca7598cb0ab3e74fdfaf56134aba486edba
Arne Brune Olsen <arne.brune.olsen@gmail.com>
2026-03-02 10:58:17 +0100

    feat(cli): add onboarding wizard for first-run setup

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,3 +1,5 @@
 // @safeclaw/cli
 export { CliAdapter } from "./adapter.js";
 export { setupChat } from "./commands/chat.js";
+export { runOnboarding } from "./commands/onboard.js";
+export type { OnboardOptions, OnboardResult } from "./commands/onboard.js";
